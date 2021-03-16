4c78b346aa0fcf9a292e45223dd23f384fa5abb4
krutik0204 <72077012+krutik0204@users.noreply.github.com>
2021-03-16 19:50:18 +0530

    Error Corrected by Krutik

--- setup.py
+++ setup.py
@@ -9,18 +9,17 @@ authors = [
         "Email" : "aadamlokhandwala786@gmail.com",
     },
     {
-        "Name" : "Murtaza Cyclewala"
-        "Email" : "murtazacyclewala27@gmail.com"
+        "Name" : "Murtaza Cyclewala",
+        "Email" : "murtazacyclewala27@gmail.com",
     },
     {
-         "Name" : "Krutik Parmar"
-        "Email" : "krutikparmar106@gmail.com"
+        "Name" : "Krutik Parmar" ,
+        "Email" : "krutikparmar106@gmail.com", 
     },
     {
-         "Name" : "Samarth Jain"
-        "Email" : "samarth16022002@gmail.com"
-    }
-    
+        "Name" : "Samarth Jain",
+        "Email" : "samarth16022002@gmail.com",
+    },    
 ]
 
 authors_string = ""
